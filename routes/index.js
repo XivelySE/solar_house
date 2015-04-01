d98e12c9201c31f2e542a40c6414b6c505220116
Fernando Rodriguez <cpurodriguez@gmail.com>
2015-04-01 17:28:19 -0300

    highcharts rendering

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,44 @@ var pg = require('../pg.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    res.render('index', {
-        title: 'Express'
+    res.sendFile(__base + 'public/dashboard-1.html')
+
+});
+
+router.get('/markers/:id/panel/current', function(req, res, next) {
+
+    pg.getMeasures('panelcurrent', function(rows) {
+
+        console.log('panelcurrent:');
+        console.log(rows);
+
+        res.send(rows);
     });
 });
 
-/* GET home page. */
-router.get('/locations/:id', function(req, res, next) {
+router.get('/markers/:id/battery/voltage', function(req, res, next) {
 
-    res.send("Location " + req.params.id);
+    pg.getMeasures('batteryvoltage', function(rows) {
+
+        console.log('batteryvoltage:');
+        console.log(rows);
+
+        res.send(rows);
+    });
 });
 
 /* GET home page. */
 router.get('/markers', function(req, res, next) {
 
-    locations = pg.getLocations();
-    res.send(locations);
-    
+    pg.getLocations(function(rows) {
+
+        console.log('locations:');
+        console.log(rows);
+
+        res.send(rows);
+
+    });
+
 });
 
 module.exports = router;
