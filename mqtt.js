f8dd6ac860add65d1631cdc9176feb7a9c155400
Fernando Rodriguez <cpurodriguez@gmail.com>
2015-04-10 11:22:58 -0300

    Changing the connection to a pool on demand

--- mqtt.js
+++ mqtt.js
@@ -3,9 +3,9 @@ var pg = require('./pg.js');
 
 var host = 'xitest.broker.xrm.xively.com';
 var port = 1883;
-var username = "f9991996-455f-420c-a83a-dc1c2690c7f6";
-var password = "6xi7vCQHiDFBH+qf/jKs8Q==";
-var topicPrefix = "/de289e01-cc13-11e4-a698-0a1f2727d969/solardemo/panel1008";
+var username = "edd7358b-9505-44b5-ab93-04ec89de2dbc";
+var password = "lW4X8e3PDUF9iOBuAIgaXQ==";
+var topicPrefix = "/de289e01-cc13-11e4-a698-0a1f2727d969/solardemo/panel0001";
 
 var client = mqtt.connect({
    host: host,
@@ -24,35 +24,40 @@ exports.connectMQTT = function(req, res) {
       client.subscribe(topicPrefix);
    });
 
-
    client.on('message', function(topic, message) {
 
       //console.log(topic);
       result = JSON.parse(message.toString());
 
-      try {
+      for (i in result) {
 
-         panelId = 1008;
-         panelValue = result[0].value;
-         panelSetting = result[0].variableName;
+         try {
 
-         if (panelSetting == 'ErrorConditionON') {
+            panelId = 1008;
+            panelValue = result[i].value;
+            panelSetting = result[i].variableName;
+
             console.log(panelId + " " + panelSetting + " " + panelValue);
+
+            if (panelSetting == 'ErrorConditionON') {
+               console.log(panelId + " " + panelSetting + " " + panelValue);
+            }
+
+            if (panelSetting == 'ErrorConditionOFF') {
+               console.log(panelId + " " + panelSetting + " " + panelValue);
+            }
+
+            if (panelSetting != 'ErrorConditionON' && panelSetting != 'SetAppliance' &&
+                  panelSetting != 'SetSource' && panelSetting != 'BatteryCharging' && panelSetting != 'ErrorConditionOFF') {
+
+               pg.saveSetting(panelId, panelSetting, panelValue);
+            }
          }
-
-         if (panelSetting == 'ErrorConditionOFF') {
-            console.log(panelId + " " + panelSetting + " " + panelValue);
-         }
-
-         if (panelSetting != 'ErrorConditionON' && panelSetting != 'SetAppliance' &&
-               panelSetting != 'SetSource' && panelSetting != 'BatteryCharging' && panelSetting != 'ErrorConditionOFF') {
-
-            pg.saveSetting(panelId, panelSetting, panelValue);
+         catch(e) {
+            console.log('Issue when receiving message');
          }
       }
-      catch(e) {
-         console.log('Issue when receiving message');
-      }
+
    });
 
 }
--- pg.js
+++ pg.js
@@ -2,32 +2,35 @@ var pg = require('pg');
 
 exports.saveSetting = function(panelId, settingType, settingValue) {
 
-    var pgClient = new pg.Client({
+    options = {
         user: "putujooxlyfpep",
         password: "uO1GsyvEHAx5lRAVTJA23X_fAD",
         database: "d2incm6jg4v8nm",
         port: 5432,
         host: "ec2-23-21-73-32.compute-1.amazonaws.com",
         ssl: true
+    }
+
+    pg.connect(options, function(err, pgClient, done) {
+
+        var query = 'INSERT INTO locationlogs(locationid, panelid, settingtype, settingvalue, timestamp) values($1, $2, $3, $4, $5)';
+
+        pgClient.query(query, ['1', panelId, settingType, settingValue, new Date()], function(err, result) {
+
+            done();
+
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            else {
+                //console.log(result);
+                pgClient.end();
+            }
+
+        });
+
     });
-
-    pgClient.connect();
-
-    var query = 'INSERT INTO locationlogs(locationid, panelid, settingtype, settingvalue, timestamp) values($1, $2, $3, $4, $5)';
-
-    pgClient.query(query, ['1', panelId, settingType, settingValue, new Date()], function(err, result) {
-
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        else {
-            //console.log(result);
-            pgClient.end();
-        }
-
-    });
-
 }
 
 exports.getMeasures = function(settingType, success, error) {
