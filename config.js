367872af4cbbeb1f240f7f34b230ae6c2c0c80cc
Gavin Berkowitz <gberkowitz@logmein.com>
2015-06-22 11:31:15 -0400

    Swapped out creds for new devices

--- config.js
+++ config.js
@@ -12,9 +12,13 @@ config.db.database = process.env.DB_NAME || 'd2incm6jg4v8nm';
 
 config.mqtt.host = process.env.MQTT_HOST || 'broker.xively.com';
 config.mqtt.port = process.env.MQTT_PORT || 1883;
-config.mqtt.username = process.env.MQTT_USERNAME || 'e2d8c2c5-43a4-4ad3-9389-ddde3494d7f7';
-config.mqtt.password = process.env.MQTT_PASSWORD || '611yAL8X+sXLFlUlhCgE1A==';
-config.mqtt.topic = process.env.MQTT_TOPIC || 'xi/blue/v1/9860a5f8-3b12-4086-963c-2830ce434835/d/5603a24f-d43d-4642-969d-1a61a63f6f13/solarpanel';
+
+//Gavin device 4
+config.mqtt.username = process.env.MQTT_USERNAME || '25357715-c40c-444a-9f9a-b08f2ac86056'; 
+config.mqtt.password = process.env.MQTT_PASSWORD || 'QBW+ULkkVXyYNETLLwxJmg==';
+
+//Gavin device 1
+config.mqtt.topic = process.env.MQTT_TOPIC || 'xi/blue/v1/9860a5f8-3b12-4086-963c-2830ce434835/d/93a1ce68-f61a-4be6-8bca-3353b81db1eb/solarpanel';
 // var topicPrefix = "/de289e01-cc13-11e4-a698-0a1f2727d969/solardemo/panel1008";
 
 module.exports = config;
